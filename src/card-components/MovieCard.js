edea6081644000f103285b9cc940fb5e5e7d2cc7
Katrina Pritchard <pritckat@gmail.com>
2018-11-09 03:53:49 +0000

    automatically backed up by learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -40,7 +40,8 @@ export default class MovieCard extends Component {
 }
 
 MovieCard.defaultProps = {
-  title: 'Unknown'
-  IMDBRating: null
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
   poster: 'default'
 }
