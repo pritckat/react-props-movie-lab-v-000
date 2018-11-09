da9bce096e430f413d8aba735c8d9f9976252218
Katrina Pritchard <pritckat@gmail.com>
2018-11-09 03:56:38 +0000

    automatically backed up by learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,8 +11,8 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    if(this.props.IMDBRating !== null) {
-      return(<img src={imgMapper[this.props.IMDBRating]}/>)
+    if (this.props.IMDBRating !== null) {
+      return (<img src={imgMapper[this.props.IMDBRating]} alt="" />)
     }
     return (<h4>No Rating Found</h4>)
   }
@@ -24,7 +24,7 @@ export default class CardBack extends Component {
         <span />
         {this.generateRatingElement()}
         <span />
-        <h5 className="genres" >{this.props.genres.join(", ")}</h5>
+        <h5 className="genres">{this.props.genres.join(", ")}</h5>
       </div>
     )
   }
